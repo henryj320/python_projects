9183dab9a26e2a21bc99384303546efc816ee57e
Henry James <59968670+henryj320@users.noreply.github.com>
2022-09-30 00:20:40 +0100

    Wrote json_file_to_dict, Updated tox.ini and setup.py and all_muscles.json (#16)

--- muscle_checker/setup.py
+++ muscle_checker/setup.py
@@ -3,9 +3,10 @@ import setuptools
 from setuptools import setup
 
 setup(
-    name="project_name",
+    name="muscle_checker",
     version="1.0",
-    description="project_description",
+    description="""Input which exercises you completed this week. The system outputs
+    which of the main muscles you missed this week and what exercises you could do to train them.""",
     author="Henry James",
     author_email="henryj320@gmail.com",
     packages=setuptools.find_packages(where="src", exclude=("tests",)),
