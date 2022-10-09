308a9c25fc6d0dab52c81aab332905829f11daac
Henry James <59968670+henryj320@users.noreply.github.com>
2022-10-09 01:36:54 +0100

    Cleaner frontend (#35)
    * Cleaner frontend
    
    * Linting

--- muscle_checker/app.py
+++ muscle_checker/app.py
@@ -24,9 +24,42 @@ def index():
     Returns:
         _type_: The index.html page.
     """
+    results_dict = run('insert_calendar_text.txt')
+
+    hit_muscle_groups = stringify("hit_muscle_groups", results_dict)
+    missed_muscle_groups = stringify("missed_muscle_groups", results_dict)
+    hit_muscles = stringify("hit_muscles", results_dict)
+
     if request.method == "POST":
         input = request.form.get("text_input")
         # return render_template('index.html', results_dict=run('tests/test_insert_calendar_text.txt'))
         results_dict = run_string(str(input))
-        return render_template('index.html', results_dict=results_dict)
-    return render_template('index.html', results_dict=run('insert_calendar_text.txt'))
+
+        hit_muscle_groups = stringify("hit_muscle_groups", results_dict)
+        missed_muscle_groups = stringify("missed_muscle_groups", results_dict)
+        hit_muscles = stringify("hit_muscles", results_dict)
+
+        return render_template('index.html', results_dict=results_dict, hit_muscle_groups=hit_muscle_groups, missed_muscle_groups=missed_muscle_groups, hit_muscles=hit_muscles)
+    return render_template('index.html', results_dict=results_dict, hit_muscle_groups=hit_muscle_groups, missed_muscle_groups=missed_muscle_groups, hit_muscles=hit_muscles)
+
+
+def stringify(index: str, dictionary: dict) -> str:
+    """Attain a clean stringified list given a dictionary and index.
+
+    Args:
+        index (str): The key for the dictionary.
+        dictionary (dict): The dictionary itself.
+
+    Returns:
+        str: The stringified list.
+    """
+    result = ""
+    for item in dictionary[index]:
+        result += item
+        result += ", "
+    if len(result) == 0:
+        result = "None"
+    else:
+        result = result[0:len(result) - 2]
+
+    return result
